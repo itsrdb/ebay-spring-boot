fc94ad9ee89124d9efa7a973495bcdbb018c6d08
Rohit Borkar <rohitborkar00@gmail.com>
2023-10-20 17:52:39 +0530

    Added Integration test for Create Product

--- src/test/java/com/itsrdb/productservice/ProductServiceApplicationTests.java
+++ src/test/java/com/itsrdb/productservice/ProductServiceApplicationTests.java
@@ -1,13 +1,62 @@
 package com.itsrdb.productservice;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.itsrdb.productservice.dto.ProductRequest;
+import com.itsrdb.productservice.repository.ProductRespository;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.testcontainers.containers.MongoDBContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+import java.math.BigDecimal;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
+@Testcontainers
+@AutoConfigureMockMvc
 class ProductServiceApplicationTests {
 
+	@Container
+	static MongoDBContainer mongoDBContainer = new MongoDBContainer("mongo:7.0.2");
+	@Autowired
+	private MockMvc mockMvc;
+	@Autowired
+	private ObjectMapper objectMapper;
+	@Autowired
+	private ProductRespository productRespository;
+
+	static void setProperties(DynamicPropertyRegistry dynamicPropertyRegistry) {
+		dynamicPropertyRegistry.add("spring.data.mongodb.uri", mongoDBContainer::getReplicaSetUrl);
+	}
+
 	@Test
-	void contextLoads() {
+	void shouldCreateProduct() throws Exception {
+		productRespository.deleteAll();
+		ProductRequest productRequest = getProductRequest();
+		String testProductRequest = objectMapper.writeValueAsString(productRequest);
+		mockMvc.perform(MockMvcRequestBuilders.post("/api/product")
+					.contentType(MediaType.APPLICATION_JSON)
+					.content(testProductRequest))
+				.andExpect(status().isCreated());
+        Assertions.assertEquals(1, productRespository.findAll().size());
+	}
+
+	private ProductRequest getProductRequest() {
+		return ProductRequest.builder()
+				.name("Pixel 7 Test")
+				.description("Google Pixel 7")
+				.price(BigDecimal.valueOf(60000))
+				.build();
 	}
 
 }
