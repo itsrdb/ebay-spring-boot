c279f61153cc69296b58f317e13b45a8dc27bd28
Rohit Borkar <rohitborkar00@gmail.com>
2023-10-20 14:38:03 +0530

    Added fuction to save product info in db

--- src/main/java/com/itsrdb/productservice/service/ProductService.java
+++ src/main/java/com/itsrdb/productservice/service/ProductService.java
@@ -4,10 +4,12 @@ import com.itsrdb.productservice.dto.ProductRequest;
 import com.itsrdb.productservice.model.Product;
 import com.itsrdb.productservice.repository.ProductRespository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class ProductService {
 
     private final ProductRespository productRespository;
@@ -18,5 +20,8 @@ public class ProductService {
                 .description(productRequest.getDescription())
                 .price(productRequest.getPrice())
                 .build();
+
+        productRespository.save(product);
+        log.info("Product {} is Saved", product.getId());
     }
 }
