8c1e2fb6e40fca5760d908e9bac77f6fb38553e7
Rohit Borkar <rohitborkar00@gmail.com>
2023-10-20 14:47:44 +0530

    Added getAllProducts method

--- src/main/java/com/itsrdb/productservice/service/ProductService.java
+++ src/main/java/com/itsrdb/productservice/service/ProductService.java
@@ -1,11 +1,14 @@
 package com.itsrdb.productservice.service;
 
 import com.itsrdb.productservice.dto.ProductRequest;
+import com.itsrdb.productservice.dto.ProductResponse;
 import com.itsrdb.productservice.model.Product;
 import com.itsrdb.productservice.repository.ProductRespository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -23,5 +26,20 @@ public class ProductService {
 
         productRespository.save(product);
         log.info("Product {} is Saved", product.getId());
+    }
+
+    public List<ProductResponse> getAllProducts() {
+        List<Product> products = productRespository.findAll();
+
+        return products.stream().map(this::mapToProductResponse).toList();
+    }
+
+    private ProductResponse mapToProductResponse(Product product) {
+        return ProductResponse.builder()
+                .id(product.getId())
+                .name(product.getName())
+                .description(product.getDescription())
+                .price(product.getPrice())
+                .build();
     }
 }
