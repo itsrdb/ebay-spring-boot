1bbd8d470ebd0ef2db85827f79b4648dc8476769
Rohit Borkar <rohitborkar00@gmail.com>
2023-10-21 18:12:10 +0530

    Added save order method to repository

--- order-service/src/main/java/com/itsrdb/orderservice/service/OrderService.java
+++ order-service/src/main/java/com/itsrdb/orderservice/service/OrderService.java
@@ -4,13 +4,18 @@ import com.itsrdb.orderservice.dto.OrderLineItemsDto;
 import com.itsrdb.orderservice.dto.OrderRequest;
 import com.itsrdb.orderservice.model.Order;
 import com.itsrdb.orderservice.model.OrderLineItems;
+import com.itsrdb.orderservice.repository.OrderRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.UUID;
 
 @Service
+@RequiredArgsConstructor
 public class OrderService {
+
+    private final OrderRepository orderRepository;
     public void placeOrder(OrderRequest orderRequest){
         Order order = new Order();
         order.setOrderNumber(UUID.randomUUID().toString());
@@ -21,6 +26,8 @@ public class OrderService {
                 .toList();
 
         order.setOrderLineItemsList(orderLineItemsList);
+
+        orderRepository.save(order);
     }
 
     private OrderLineItems mapToDto(OrderLineItemsDto orderLineItemsDto){
