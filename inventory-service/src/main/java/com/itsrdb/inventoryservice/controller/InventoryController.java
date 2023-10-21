eb9ccf871d73f30d3c5342777dae6cd69f37fd9b
Rohit Borkar <rohitborkar00@gmail.com>
2023-10-21 19:33:22 +0530

    Added isInStock method to InventoryController

--- inventory-service/src/main/java/com/itsrdb/inventoryservice/controller/InventoryController.java
+++ inventory-service/src/main/java/com/itsrdb/inventoryservice/controller/InventoryController.java
@@ -1,15 +1,20 @@
 package com.itsrdb.inventoryservice.controller;
 
+import com.itsrdb.inventoryservice.service.InventoryService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/inventory")
+@RequiredArgsConstructor
 public class InventoryController {
+
+    private final InventoryService inventoryService;
 
     @GetMapping("/{sku-code}")
     @ResponseStatus(HttpStatus.OK)
     public boolean isInStock(@PathVariable("sku-code") String skuCode) {
-
+        return inventoryService.isInStock(skuCode);
     }
 }
