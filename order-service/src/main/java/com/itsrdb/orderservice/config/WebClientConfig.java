64da4e920d90e78b1ff4f0bd08a43c81ca2e955f
Rohit Borkar <rohitborkar00@gmail.com>
2023-10-24 11:28:09 +0530

    Added WebClient config to work with multiple instances

--- order-service/src/main/java/com/itsrdb/orderservice/config/WebClientConfig.java
+++ order-service/src/main/java/com/itsrdb/orderservice/config/WebClientConfig.java
@@ -1,5 +1,6 @@
 package com.itsrdb.orderservice.config;
 
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.function.client.WebClient;
@@ -11,7 +12,8 @@ import org.springframework.web.reactive.function.client.WebClient;
 public class WebClientConfig {
 
     @Bean
-    public WebClient webClient() {
-        return WebClient.builder().build();
+    @LoadBalanced
+    public WebClient.Builder webClientBuilder() {
+        return WebClient.builder();
     }
 }
